ff42e9fabcc8f797216b91982ac2dd975943b459
Joe Borg <joseph.borg@canonical.com>
2020-04-29 15:48:41 -0400

    Bump to version 0.1.0

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='conctl',
-    version='0.0.24',
+    version='0.1.0',
     url='https://github.com/joedborg/conctl',
     license='Apache License 2.0',
     author='Joe Borg',
-    author_email='joe@josephb.org',
+    author_email='joseph.borg@canonical.com',
     description='Drive Containerd and Docker from one CLI',
     packages=['conctl', 'conctl/bin'],
     install_requires=[
