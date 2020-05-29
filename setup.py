adaa3e2b82cba19617785ed5ce8e4f98966d7c64
Joe Borg <joseph.borg@canonical.com>
2020-05-29 19:02:18 -0400

    v0.1.2 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='conctl-py35',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/joedborg/conctl',
     license='Apache License 2.0',
     author='Joe Borg',
